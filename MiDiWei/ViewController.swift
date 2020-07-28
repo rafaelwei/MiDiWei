77a946bb4c6593acb125131b326381cd24690765
Pedro Miranda <pgmirandafig@gmail.com>
2020-07-28 16:02:51 -0300

    Tab bars com as 3 telas

--- MiDiWei/ViewController.swift
+++ MiDiWei/ViewController.swift
@@ -18,3 +18,22 @@ class ViewController: UIViewController {
 
 }
 
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
