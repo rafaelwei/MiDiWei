1b33d851d5a1b1ad80648d6309c433e96edaf783
Larissa Dinix <lariediniz@gmail.com>
2020-07-31 14:53:34 -0300

    inicio da home

---
+++ MiDiWei/HomeViewController.swift
@@ -0,0 +1,14 @@
+//
+//  HomeViewController.swift
+//  MiDiWei
+//
+//  Created by Larissa Diniz on 31/07/20.
+//  Copyright © 2020 Rafael Wei. All rights reserved.
+//
+
+import Foundation
+
+struct Home {
+    
+    let nome : String //nome do mentor
+}
