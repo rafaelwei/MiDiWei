035edcd4a7f0116ae1052f849a492d60fc82cdb6
Pedro Miranda <pgmirandafig@gmail.com>
2020-08-04 15:06:26 -0300

    Atualizações tableview tela de perfil

--- MiDiWei/PerfilViewController.swift
+++ MiDiWei/PerfilViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 
-class PerfilViewController: UIViewController {
+class PerfilViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var nameLabel: UILabel!
     
@@ -19,14 +19,51 @@ class PerfilViewController: UIViewController {
 
     @IBOutlet weak var profileImage: UIImageView!
     
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        nameLabel.text = "Pedro Miranda"
-        functionLabel.text = "Artista 3D"
-        courseLabel.text = "Design - 6º período"
-        profileImage.image = #imageLiteral(resourceName: "Naruto_Uzumaki_(Parte_I_-_HD).png")
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        tableView.register(UINib(nibName: "TableViewCell", bundle: nil), forCellReuseIdentifier: "Sobre")
+
         // Do any additional setup after loading the view.
     }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 4
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+      return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        if indexPath.row == 0{
+//           let cell = tableView.dequeueReusableCell(withIdentifier: "Sobre", for: indexPath) as? TableViewCell
+//            return cell!
+//        } else if indexPath.row == 1{
+//            let cell = tableView.dequeueReusableCell(withIdentifier: "Experiencia", for: indexPath) as? MentorTableViewCell
+//            return cell!
+//        } else if indexPath.row == 2{
+//
+//        }
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Sobre", for: indexPath) as? TableViewCell
+                  return cell!
+//        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.row == 0{
+            return 300
+        }
+        return 150
+
+     
+
+    }
+    
+   // func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        //
+    //}
 
 
 }
---
+++ MiDiWei/TableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  TableViewCell.swift
+//  MiDiWei
+//
+//  Created by Pedro Guilherme Figueiredo Miranda on 03/08/20.
+//  Copyright © 2020 Rafael Wei. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        print(#function)
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
