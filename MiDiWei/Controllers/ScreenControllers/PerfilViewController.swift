46721a2c8cfa5b8bcb2cd5b2e01d10380daca2ef
rafaelwei <rafaelwei07@gmail.com>
2020-08-26 13:19:24 -0300

    Tela de perfil quase concluida

--- MiDiWei/Controllers/ScreenControllers/PerfilViewController.swift
+++ MiDiWei/Controllers/ScreenControllers/PerfilViewController.swift
@@ -12,11 +12,9 @@ import UIKit
 class PerfilViewController: UIViewController {
 
     @IBOutlet weak var nameLabel: UILabel!
-    
     @IBOutlet weak var functionLabel: UILabel!
-    
     @IBOutlet weak var courseLabel: UILabel!
-
+    
     @IBOutlet weak var profileImage: UIImageView!
     
     @IBOutlet weak var tableView: UITableView!
@@ -36,11 +34,26 @@ class PerfilViewController: UIViewController {
         self.tableView.dataSource = self
         self.tableView.delegate = self
         
+        //nameLabel.text = "Pedro Miranda"
+        
+        //functionLabel.text = "Artista 3D"
+        
+        //courseLabel.text = "Design - 6º período"
+        
         let nib = UINib(nibName: "AboutTableViewCell", bundle: nil)
         tableView.register(nib, forCellReuseIdentifier: "Sobre")
         
-        tableView.rowHeight = UITableView.automaticDimension
-        tableView.estimatedRowHeight = 44
+        let nib2 = UINib(nibName: "ExperienceTableViewCell", bundle: nil)
+        tableView.register(nib2, forCellReuseIdentifier: "Experiencia")
+        
+        let nib3 = UINib(nibName: "PlaceHolderTableViewCell", bundle: nil)
+        tableView.register(nib3, forCellReuseIdentifier: "PlaceHolder")
+        
+        let nib4 = UINib(nibName: "JobsTableViewCell", bundle: nil)
+        tableView.register(nib4, forCellReuseIdentifier: "Jobs")
+        
+        //tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 45
 
         // Do any additional setup after loading the view.
         phrases.append(one);
@@ -55,20 +68,75 @@ class PerfilViewController: UIViewController {
 extension PerfilViewController: UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return phrases.count
+        return 7
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Sobre", for: indexPath) as! AboutTableViewCell
-        
-        cell.aboutLabel.numberOfLines = 0
-        cell.aboutLabel.text = phrases[indexPath.row]
-        
-        return cell
+        if(indexPath.row == 0) {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "PlaceHolder", for: indexPath) as! PlaceHolderTableViewCell
+            
+            cell.separatorInset = UIEdgeInsets.init(top: 0, left: 0, bottom: 0, right: .greatestFiniteMagnitude)
+            cell.directionalLayoutMargins = .zero
+            
+            cell.myLabel.text = "Sobre"
+            
+            return cell
+        }
+        else if(indexPath.row == 1) {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "Sobre", for: indexPath) as! AboutTableViewCell
+            
+            cell.aboutLabel.numberOfLines = 0
+            cell.aboutLabel.text = phrases[indexPath.row-1]
+            
+            return cell
+        }
+        else if(indexPath.row == 2){
+            let cell = tableView.dequeueReusableCell(withIdentifier: "PlaceHolder", for: indexPath) as! PlaceHolderTableViewCell
+            
+            cell.separatorInset = UIEdgeInsets.init(top: 0, left: 0, bottom: 0, right: .greatestFiniteMagnitude)
+            cell.directionalLayoutMargins = .zero
+            
+            cell.myLabel.text = "Experiência"
+            
+            return cell
+        }
+        else if(indexPath.row == 5) {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "PlaceHolder", for: indexPath) as! PlaceHolderTableViewCell
+            
+            cell.separatorInset = UIEdgeInsets.init(top: 0, left: 0, bottom: 0, right: .greatestFiniteMagnitude)
+            cell.directionalLayoutMargins = .zero
+            
+            cell.myLabel.text = "Trabalhos significatvos"
+            
+            return cell
+        }
+        else if(indexPath.row == 6){
+            let cell = tableView.dequeueReusableCell(withIdentifier: "Jobs", for: indexPath) as! JobsTableViewCell
+            
+            return cell
+        }
+        else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "Experiencia", for: indexPath) as! ExperienceTableViewCell
+            
+            cell.myLabel.text = phrases[indexPath.row-2]
+            
+            if(indexPath.row % 2 == 1){
+                cell.myIcon.image = UIImage(named: "Valley_of_the_End")
+            }
+            
+            return cell
+        }
+    
     }
-     
-    // func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-         //
-     //}
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if(indexPath.row == 6){
+            return 150
+        }
+        if(indexPath.row > 0 && indexPath.row != 2) {
+            return 100
+        }
+        return UITableView.automaticDimension
+    }
 
 }
---
+++ MiDiWei/Controllers/TableViewCellControllers/PlaceHolderTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  PlaceHolderTableViewCell.swift
+//  MiDiWei
+//
+//  Created by Rafael Wei on 26/08/20.
+//  Copyright © 2020 Rafael Wei. All rights reserved.
+//
+
+import UIKit
+
+class PlaceHolderTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var myLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
